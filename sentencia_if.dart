bab8863fb6144c7cfa27f7cc9ef95d14fb97ac03
2021 DSM31 Angel Flores Carlos <angelfc.contact@gmail.com>
2021-09-17 11:50:43 -0500

    sentencia if

---
+++ sentencia_if.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  String sexo = 'M';
+  if ('F' == sexo) {
+    print('Bienvenida');
+  } else if ('M' == sexo) {
+    print('Bienvenido');
+  } else {
+    print('Hola');
+  }
+  if ('0' == sexo) print('Ok, respetable');
+}
