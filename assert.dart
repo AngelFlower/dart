8e66d3db735758aafec45d3db76d75f34bb700ed
2021 DSM31 Angel Flores Carlos <angelfc.contact@gmail.com>
2021-09-23 09:43:25 -0500

    assert

---
+++ assert.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  double numerador = 10, denominador = 0;
+
+  assert(denominador != denominador, 'No podre hacer la división');
+  print('División = ${numerador / denominador}');
+  /*
+  if (0 != denominador) {
+    print('División = ${numerador / denominador}');
+  } else {
+    print('La división entre 0 no está definida');
+  }
+  */
+}
